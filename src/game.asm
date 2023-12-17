724d513d75d7a03ae898ce2a7846fabb3978fefb
sazonovitas <itassudo@gmail.com>
2023-12-17 17:21:19 +0300

    chasing light debug info

--- src/game.asm
+++ src/game.asm
@@ -15,7 +15,7 @@ proc Game.Game uses edi ebx,\
     cmp     [pl_help], true
     jne     @F
 
-    stdcall Game.Help
+    stdcall Game.Help, [pPlayer]
 
     @@:
 
@@ -32,16 +32,19 @@ proc Game.Game uses edi ebx,\
 
     .SkipTimerUpdate:
 
+    stdcall Draw.Text, -35.0, 34.0, 1.0, 1.0, 1.0, Help.FPS, Help.FPSLen
+    stdcall Draw.Text, -33.8, 34.0, 0.0, 1.0, 0.0, Help.FPSCnt, Help.FPSCntLen
+
 .Ret:
     ret
 endp
 
-proc Game.Help
+proc Game.Help uses edi esi ebx,\
+    pPlayer
 
-    stdcall Draw.Text, -35.0, 34.0, 1.0, 1.0, 1.0, Help.FPS, Help.FPSLen
-
-
-    stdcall Draw.Text, -33.8, 34.0, 0.0, 1.0, 0.0, Help.FPSCnt, Help.FPSCntLen
+    locals 
+        tmp     Vector3     ?
+    endl
 
     ; Wallking debug info
     stdcall Draw.Text, -35.0, 33.0, 0.2, 0.4, 1.0, Help.DebugInfo, Help.DebugInfoLen
@@ -126,6 +129,23 @@ proc Game.Help
     stdcall Draw.Text, -33.5, 12.0, 1.0, 1.0, 1.0, Help.ChooseLightColor, Help.ChooseLightColorLen
     stdcall Draw.Text, -33.5, 11.0, 1.0, 1.0, 1.0, Help.ChangeLightIntensity, Help.ChangeLightIntensityLen
     stdcall Draw.Text, -33.5, 10.0, 1.0, 1.0, 1.0, Help.SaveLevel, Help.SaveLevelLen
+
+    ; Chasing Light
+    stdcall Draw.Text, -34.0, 9.0, 1.0, 1.0, 1.0, Help.ChasingLight, Help.ChasingLightLen
+    stdcall Draw.Text, -33.5, 8.0, 1.0, 1.0, 1.0, Help.ChasingLightRGB, Help.ChasingLightRGBLen
+
+    lea     ebx, [tmp]
+    mov     edi, [pPlayer]
+    mov     esi, [edi + Player.pLevel]
+    mov     eax, [edi + Player.offsetChasingLight]
+    mov     edx, sizeLight
+    mul     edx
+    add     eax, [esi + Level.pLightsMap]
+    add     eax, colorLightOffset
+    stdcall Vector3.Copy, ebx, eax
+    stdcall Vector3.Normalize, ebx
+    stdcall Draw.Text, -32.0, 8.0, dword [ebx], dword [ebx + 4],\
+                                dword [ebx + 8], Help.RGBColor, Help.RGBColorLen
 
     ; ============ Player =============
     ret
